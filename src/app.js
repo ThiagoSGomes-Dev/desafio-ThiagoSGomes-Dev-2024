e2987f23edceff31241306f71556add2d65740d9
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-09-09 07:40:38 -0300

    feat: adicionar lista para retornar recintos e animais

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { RecintosZoo } from "./recintos-zoo.js";
+
+const recintosZoo = new RecintosZoo();
+
+console.table(recintosZoo.analisaRecintos("MACACO", 2));
--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,8 +1,38 @@
 class RecintosZoo {
+  constructor() {
+    this.recintos = [
+      {
+        numero: 1,
+        bioma: "savana",
+        tamanho: 10,
+        animais: {
+          macaco: 3,
+          leão: 1,
+        },
+      },
+      { numero: 2, bioma: "floresta", tamanho: 5, animais: {} },
+      { numero: 3, bioma: "savana e rio", tamanho: 7, animais: { gazela: 1 } },
+      { numero: 4, bioma: "rio", tamanho: 9, animais: { leão: 1 } },
+    ];
 
-    analisaRecintos(animal, quantidade) {
-    }
+    // this.recintos[1].animais.jaguar = 2;
 
+    this.animais = {
+      leao: { tamanho: 3, bioma: "savana" },
+      leopardo: { tamanho: 2, bioma: "savana" },
+      crocodilo: { tamanho: 1, bioma: "rio" },
+      macaco: { tamanho: 2, biomas: ["savana", "floresta"] },
+      gazela: { tamanho: 4, biomas: ["savana", "rio"] },
+    };
+
+    // this.animais.leao.tamanho = 10;
+  }
+
+  analisaRecintos(animal, quantidade) {
+    const animaisInfo = this.animais;
+
+    return animaisInfo;
+  }
 }
 
 export { RecintosZoo as RecintosZoo };
