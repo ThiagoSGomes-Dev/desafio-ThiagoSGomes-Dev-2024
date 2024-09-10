002fd8ebc79dd80a43e4ce3ac07a95bb9daf17cb
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-09-10 11:23:24 -0300

    fix: ajuste nos dados da classe Recinto

--- src/model/recintos.js
+++ src/model/recintos.js
@@ -1,18 +1,11 @@
 class Recinto {
   constructor() {
     this.recintos = [
-      {
-        numero: 1,
-        bioma: "savana",
-        tamanho: 10,
-        animais: {
-          macaco: 3,
-          leão: 1,
-        },
-      },
+      { numero: 1, bioma: "savana", tamanho: 10, animais: { macaco: 3 } },
       { numero: 2, bioma: "floresta", tamanho: 5, animais: {} },
       { numero: 3, bioma: "savana e rio", tamanho: 7, animais: { gazela: 1 } },
-      { numero: 4, bioma: "rio", tamanho: 9, animais: { leão: 1 } },
+      { numero: 4, bioma: "rio", tamanho: 8, animais: {} },
+      { numero: 5, bioma: "savana", tamanho: 9, animais: { leão: 1 } },
     ];
   }
 
