327aae700e314a6489ad2e0fe310924c3d93e00b
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-09-12 19:12:22 -0300

    fix: ajuste nos arquivos de test

--- src/app.js
+++ src/app.js
@@ -2,4 +2,5 @@ import { RecintosZoo } from "./recintos-zoo.js";
 
 const recintosZoo = new RecintosZoo();
 
-console.log(recintosZoo.analisaRecintos("MACACO", 1));
+// console.log(recintosZoo.analisaRecintos("LEAO", 1));
+console.log(recintosZoo.analisaRecintos("MACACO", 2));
