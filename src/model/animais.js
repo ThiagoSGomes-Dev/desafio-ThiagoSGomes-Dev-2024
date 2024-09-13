21f53fbfe8955a56f55f5f6de21be4f0071e6a28
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-09-12 21:16:25 -0300

    fix: corrigindo erro nos biomas da classe Animais

--- src/model/animais.js
+++ src/model/animais.js
@@ -4,9 +4,9 @@ class Animais {
       leao: { tamanho: 3, bioma: "savana" },
       leopardo: { tamanho: 2, bioma: "savana" },
       crocodilo: { tamanho: 3, bioma: "rio" },
-      macaco: { tamanho: 1, biomas: ["savana", "floresta"] },
+      macaco: { tamanho: 1, bioma: ["savana", "floresta"] },
       gazela: { tamanho: 2, bioma: "savana" },
-      hipopotamo: { tamanho: 4, biomas: ["savana", "rio"] },
+      hipopotamo: { tamanho: 4, bioma: ["savana", "rio"] },
     };
   }
 
